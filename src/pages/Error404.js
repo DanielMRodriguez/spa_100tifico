629fb46eb0ee029e66ad3910edb8de8760e62371
Luism <luis.mendoza.0896@gmail.com>
2020-02-15 00:08:47 -0600

    config inicial

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+const Error404 = () => {
+    const view = `
+        <div class="Error-404">
+            <h2>
+                No se encontro
+            </h2>
+        </div>
+    `;
+    return view
+}
+
+export default Error404;

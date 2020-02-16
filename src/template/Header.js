73327d2978f29c9e5eac99aad8f4c92ca5e3e16f
Luism <luis.mendoza.0896@gmail.com>
2020-02-16 16:33:54 -0600

    agregue ruta para home en githubpage

--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="https://danielmrodriguez.github.io/spa_100tifico/">
                         100ntifiko
                     </a>
                 </h1>
